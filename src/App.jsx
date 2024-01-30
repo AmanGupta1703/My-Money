8a47da7be03fe3d4c8f97880bd94c1a6bad9492d
AmanGupta1703 <guptakunalx@gmail.com>
2024-01-30 17:02:31 +0530

    Add new pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// pages
+import { Home, Login, Signup } from './pages';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from './home/Home.jsx';
+import Login from './login/Login.jsx';
+import Signup from './signup/Signup.jsx';
+
+export { Home, Login, Signup };
