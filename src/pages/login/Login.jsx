0e52af36096c3db0cc4ed93bbd257effc20620ff
AmanGupta1703 <guptakunalx@gmail.com>
2024-01-30 17:48:14 +0530

    Add login form with email and password fields

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,39 @@
+// styles
+import { useState } from 'react';
 import styles from './Login.module.css';
 
 function Login() {
+  const [email, setEmail] = useState('amangupta@gmail.com');
+  const [password, setPassword] = useState('aman@123');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(email, password);
+  }
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <form className={styles['login-form']} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
+  );
 }
 
-export default Login
-
+export default Login;
