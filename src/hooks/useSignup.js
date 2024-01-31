aa811841e09939c2205eb3e94e9e2bcd381ce11b
AmanGupta1703 <guptakunalx@gmail.com>
2024-01-31 10:13:27 +0530

    Add useSignup hook for user signup

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,37 @@
+const { useState } = require('react');
+
+// firestore
+import { projectAuth } from '../firebase/config';
+
+function useSignup() {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  async function signup(email, password, displayName) {
+    try {
+      // signup the user (with email and password)
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user); // created user
+
+      if (!res) {
+        throw new Error('Could not complete signup');
+      }
+
+      // add `displayName` to the `user` object
+      await res.user.updateProfile({ displayName });
+
+      setError(null);
+      setIsPending(false);
+    } catch (error) {
+      setError(error.message);
+      setIsPending(false);
+    }
+  }
+
+  return { error, isPending, signup };
+}
+
+export { useSignup };
