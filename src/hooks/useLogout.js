ef77589b64d5c6aced4db9b8f606aac3caeebd41
AmanGupta1703 <guptakunalx@gmail.com>
2024-02-06 17:01:22 +0530

    Add logout functionality to AuthContext and useLogout hook

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+// firbase
+import { projectAuth } from '../firebase/config';
+
+// auth context hook
+import { useAuthContext } from './useAuthContext';
+
+function useLogout() {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const { dispatch } = useAuthContext();
+
+  async function logout() {
+    try {
+      setError(null);
+      setIsPending(true);
+
+      await projectAuth.signOut();
+
+      // dispatch a logout action
+      dispatch({ type: 'LOGOUT' });
+
+      setError(null);
+      setIsPending(false);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+      setIsPending(false);
+    }
+  }
+
+  return { error, isPending, logout };
+}
